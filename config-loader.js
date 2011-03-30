b295c7e1a426acfff539f728e106117740eecb27
Mark Wubben <mark@novemberborn.net>
2011-03-30 17:01:49 +0100

    Rewrite loader.
    Now exposes the load method directly when you required the package. Resolves relative to directory of main module or the current working directory if no main module could be found. Support commented JSON.

--- config-loader.js
+++ config-loader.js
@@ -1,31 +1,83 @@
 var fs = require("fs"),
     path = require("path");
 
-exports.load = function(relpath){
-  var dir = path.dirname(relpath);
-  var name = path.basename(relpath, ".js");
-  
-  var json;
-  var src, tried = [];
-  try{
-    // Try loading from the dir itself
-    var src = path.join(dir, "config", name + ".json");
-    tried.push(src);
-    json = fs.readFileSync(src);
-  }catch(e){
-    // Try loading from the parent directory
-    src = path.join(dir, "..", "config", name + ".json");
-    tried.push(src);
-    json = fs.readFileSync(src);
-  }finally{
-    if(!json){
-      throw new Error("Could not load configuration file, tried:\n" + tried.join("\n"));
+module.exports = exports = function(name){
+  var configPaths = [];
+  if(process.mainModule){
+    configPaths = process.mainModule.paths.map(function(dir){
+      return dir.replace("node_modules", "config");
+    });
+  }else{
+    var dir = process.cwd();
+    while(dir !== "/"){
+      configPaths.push(path.join(dir, "config"));
+      dir = path.dirname(dir);
     }
+    configPaths.push(path.join(dir, "config"));
+  }
+  
+  var filename = name + ".json";
+  var src;
+  var json;
+  var tried = [];
+  while(configPaths.length && !json){
+    src = path.join(configPaths.shift(), filename);
+    tried.push(src);
     
     try{
-      return JSON.parse(json);
-    }catch(e){
-      throw new Error("Error parsing " + tried[tried.length - 1]);
-    }
+      json = fs.readFileSync(src, "utf8");
+    }catch(e){}
+  }
+  
+  if(!json){
+    throw new Error("Could not load configuration file, tried:\n" + tried.join("\n"));
+  }
+  
+  try{
+    var config = JSON.parse(stripComments(json));
+    Object.defineProperty(config, "-src-", {
+      value: src,
+      enumerable: false
+    });
+    return config;
+  }catch(e){
+    throw new Error("Error parsing " + src);
   }
 };
+
+var NoComment = 0,
+    SingleLineComment = 1,
+    MultiLineComment = 2;
+function stripComments(json){
+  var stripped = "";
+  var inComment = NoComment, inString = false;
+  
+  for(var i = 0; i < json.length; i++){
+    var current = json[i];
+    var skip = inComment === SingleLineComment || inComment === MultiLineComment;
+    
+    if(inComment === NoComment && current === "\"" && json[i - 1] !== "\\"){
+      inString = !inString;
+    }
+    
+    if(!inString){
+      var next = json[i + 1];
+      if(!inComment && current === "/" && (next === "/" || next === "*")){
+        inComment = next === "/" ? SingleLineComment : MultiLineComment;
+        skip = true;
+        i++;
+      }else if(inComment === SingleLineComment && current === "\n"){
+        inComment = NoComment;
+      }else if(inComment === MultiLineComment && current === "*" &&  next === "/"){
+        inComment = NoComment;
+        i++;
+      }
+    }
+    
+    if(!skip){
+      stripped += current;
+    }
+  }
+  
+  return stripped;
+}
